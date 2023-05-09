44d36c7f0e79bbd906269edb6f5d711f25948737
nathangero <nathangero@yahoo.com>
2023-05-09 14:51:37 -0700

    initialized firebase

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { }
-
+const admin = require('firebase-admin');
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,10 +9,21 @@ import { }
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+const firebaseConfig = {
+    apiKey: process.env.firebase_apiKey,
+    authDomain: process.env.firebase_authDomain,
+    projectId: process.env.firebase_projectId,
+    storageBucket: process.env.firebase_storageBucket,
+    messagingSenderId: process.env.firebase_messagingSenderId,
+    appId: process.env.firebase_appId,
+    measurementId: process.env.firebase_measurementId
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+
 const db = app.database()
 
 export default db
