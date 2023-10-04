509a6e9b326688dee17837fd372a146e0b4b549f
nathangero <nathangero@yahoo.com>
2023-10-04 15:47:13 -0700

    refactor nav bar with bootstrap

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="body">
+  <div>
     <transition name="slide-up" mode="out-in">
       <div v-if="isShowingCharacterList">
         <side-menu @click="toggleMenu"></side-menu>
         
         <h1>{{ getUserInfo.name }}'s characters</h1>
-        <button class="button-open-modal" @click="toggleModalForCreateCharacter">Create Character</button>
+        <button @click="toggleModalForCreateCharacter">Create Character</button>
 
         <hr v-if="!isMenuOpen">
         <hr v-if="isMenuOpen" style="visibility: hidden ;" >
@@ -14,6 +14,22 @@
         <template v-if="getDictionarySize(store.getters.getUserCharacters) > 0">
           <character-summary :list-of-characters="store.getters.getUserCharacters" @openModal="toggleModalForViewCharacter"></character-summary>
         </template>
+
+        <nav>
+          <ul class="nav justify-content-between fixed-bottom text-capitalize fs-5 custom-navbar">
+            <li class="nav-item" @click="navigateTo(ROUTER_NAMES.CAMPAIGNS)">
+              <a class="nav-link text-dark text-lg-center p-3">{{ ROUTER_NAMES.CAMPAIGNS }}</a>
+            </li>
+            
+            <li class="nav-item" @click="navigateTo(ROUTER_NAMES.CHARACTERS)">
+              <a class="nav-link text-dark p-3">{{ ROUTER_NAMES.CHARACTERS }}</a>
+            </li>
+
+            <li class="nav-item" @click="navigateTo(ROUTER_NAMES.SESSIONS)">
+              <a class="nav-link text-dark p-3">{{ ROUTER_NAMES.SESSIONS }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </transition>
 
@@ -30,23 +46,6 @@
         <character-create v-if="isModalNewCharacterOpen" @close="toggleModalForCreateCharacter" @created-character="toggleModalForCreateCharacter"></character-create>
       </template>
     </transition>
-
-    <!-- Bottom Navigation Bar -->
-    <!-- <nav class="bottom-navigation" v-if="isNavBarOpen">
-      <ul>
-        <li @click="navigateTo(ROUTER_NAMES.CAMPAIGNS)" :class="{ active: currentRoute === ROUTER_NAMES.CAMPAIGNS }">
-          <i class="fas fa-campaigns"></i>
-          {{ ROUTER_NAMES.CAMPAIGNS.charAt(0).toUpperCase() + ROUTER_NAMES.CAMPAIGNS.slice(1) }}
-        </li>
-        <li @click="navigateTo(ROUTER_NAMES.CHARACTERS)" :class="{ active: currentRoute === ROUTER_NAMES.CHARACTERS }">
-          <i class="fas fa-characters"></i>
-          {{ ROUTER_NAMES.CHARACTERS.charAt(0).toUpperCase() + ROUTER_NAMES.CHARACTERS.slice(1) }}
-        </li><li @click="navigateTo(ROUTER_NAMES.SESSIONS)" :class="{ active: currentRoute === ROUTER_NAMES.SESSIONS }">
-          <i class="fas fa-sessions"></i>
-          {{ ROUTER_NAMES.SESSIONS.charAt(0).toUpperCase() + ROUTER_NAMES.SESSIONS.slice(1) }}
-        </li>
-      </ul>
-    </nav> -->
     
   </div>
 </template>
@@ -185,11 +184,23 @@ export default {
 
 <style scoped>
 @import '../styles/reset.css';
+@import "../styles/colors.css";
 /* @import '../styles/character-summary.css'; */
 /* @import '../styles/popup.css'; */
 /* @import '../styles/transitions.css'; */
 
-body {
-  background-color: purple;
+button {
+  color: var(--white);
+  padding: 5px 10px;
+  margin-top: 10px;
+  border: none;
+  border-radius: var(--border-radius);
+  font-size: var(--stat-font-size);
+  background-color: var(--dimgray);
 }
+
+.custom-navbar {
+  background-color: var(--light-gray);
+}
+
 </style>
