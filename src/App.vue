f499ee67d543a98f75ab55d01326a50502e6b824
nathangero <nathangero@yahoo.com>
2023-10-05 14:43:00 -0700

    implement changing mobile status bar theme

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,34 @@ export default {
   components: {
     
   },
+  data() {
+    return {
+      darkModeMediaQuery: window.matchMedia('(prefers-color-scheme: dark)'),
+      isDarkMode: true,
+    }
+  },
   computed: {
     ...mapState(['isLoggedIn'])
   },
   created() {
-    
     document.title = "DND Save Data";
+
+    this.isDarkMode = this.darkModeMediaQuery.matches;
+
+    // Make the status bar on mobile match the background color
+    const metaTag = document.createElement("meta");
+    metaTag.name = "theme-color";
+    metaTag.content = this.isDarkMode ? "#000000" : "#ffffff"; // Make black or white 
+    document.head.children[3]
+    document.head.appendChild(metaTag);
+  },
+  mounted: function() {
+    this.darkModeMediaQuery.addEventListener("change", () => {
+      this.isDarkMode = this.darkModeMediaQuery.matches;
+      console.log("is dark mode?", this.isDarkMode)
+
+      document.querySelector('meta[name="theme-color"]').setAttribute("content", this.isDarkMode ? "#000000" : "#ffffff")
+    })
   }
 }
 </script>
